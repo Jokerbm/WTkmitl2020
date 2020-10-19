3886430b4c16d65dd2157484b14588f695969593
Lalitwadee <rari363970@gmail.com>
2020-10-19 23:10:00 +0700

    break a day

--- storylog/src/components/index.js
+++ storylog/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Home } from "./Home";
 export { default as About } from "./About";
 export { default as Contact } from "./Contact";
 export { default as Banner } from "./Banner";
+export { default as Login } from "./Login";
