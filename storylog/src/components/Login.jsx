41a6cb5219c0bbc9818e1257791565cc8e6f56b2
Lalitwadee <rari363970@gmail.com>
2020-10-23 02:31:52 +0700

    login popup ok

--- storylog/src/components/Login.jsx
+++ storylog/src/components/Login.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+
 function Login() {
     return (
-        <div className="footer">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal
+      <div className="Login">
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Login
             </button>
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="card-popup login">
+                            <div className="popup-title th-font2" id="exampleModalLabel"><h4 className="p-2"><b>LOGIN TO STORYLOG</b></h4></div>
+                        </div>
+                        <div className="register-form center">
+                            <button className="facebook-login-button mb-3">
+                                <img className="facebook-login-button-icon" width="24" height="24" src={process.env.PUBLIC_URL + 'img/facebook.png'} alt="test">
+                                </img><span className="th-font2"> Login with Facebook</span>
                             </button>
+                            <div className="text th-font2">เราใช้การล็อคอินผ่าน
+                                Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+                                อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+                            </div>
+                            <div className="tabs">
+                                <div className="tabs-text login th-font2">OR</div>
+                                <div className="tabs-line login"></div>
+                            </div>
+                            <div className="w-form">
+                                <input className="w-input input th-font2 mb-4" id="input_username_login" type="text"
+                                       placeholder="Penname (Username)"></input>
+                                <input className="w-input input th-font2" id="input_password_login" type="password"
+                                       placeholder="Password"></input>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                        <div className="popup-footer">
+                            <button className="button" data-dismiss="modal">Cancel</button>
+                            <button className="button green">Login</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+      </div>
     );
 }
 export default Login;
