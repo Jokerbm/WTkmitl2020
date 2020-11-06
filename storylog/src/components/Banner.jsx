84ed24e44f4108625b4074f57b6371362d97dd1f
Lalitwadee <rari363970@gmail.com>
2020-11-06 16:41:07 +0700

    Revert "Merge branch 'DEV_Tusneem' into DEV_Rayrai"
    This reverts commit a795bd55e274a776689926d9dd79858e183c7003, reversing
    changes made to 943ad087fedc9d4a479d6d4834d4988e9fe3bed9.

--- storylog/src/components/Banner.jsx
+++ storylog/src/components/Banner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="col-12" style={{paddingLeft: "0px",paddingRight: "0px"}}>
+    <div class="col-12" style={{paddingLeft: "0px",paddingRight: "0px"}}>
     <img
-        className="img-fluid rounded mb-4 mb-lg-0"
+        class="img-fluid rounded mb-4 mb-lg-0"
         src="https://storylog.co/assets/1UHhBMl.jpg"
         alt=""
     />
--- storylog/src/components/Footer.jsx
+++ storylog/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer classNamem="py-5 bg-dark fixed-bottom">
-        <div classNamem="container">
-          <p classNamem="m-0 text-center text-white">
+      <footer class="py-5 bg-dark fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
             Copyright &copy; Your Website 2020
           </p>
         </div>
