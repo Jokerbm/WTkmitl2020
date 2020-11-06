738b3ff502c78f61915eb07230c836e848e0adae
Lalitwadee <rari363970@gmail.com>
2020-11-06 16:49:56 +0700

    Revert "Revert "Merge branch 'DEV_Tusneem' into DEV_Rayrai""
    This reverts commit 84ed24e44f4108625b4074f57b6371362d97dd1f.

--- storylog/src/components/Banner.jsx
+++ storylog/src/components/Banner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Banner() {
   return (
-    <div class="col-12" style={{paddingLeft: "0px",paddingRight: "0px"}}>
+    <div className="col-12" style={{paddingLeft: "0px",paddingRight: "0px"}}>
     <img
-        class="img-fluid rounded mb-4 mb-lg-0"
+        className="img-fluid rounded mb-4 mb-lg-0"
         src="https://storylog.co/assets/1UHhBMl.jpg"
         alt=""
     />
--- storylog/src/components/Home.jsx
+++ storylog/src/components/Home.jsx
@@ -30,13 +30,68 @@ function Home() {
                     <div>
                         <div id="feedHighlight">
                             {/* information about blog */}
-                            {body()}
-                            <StaffPickBorder/>
-                            <StaffPickBorder/>
-                            {/*<ReadListBorder />*/}
-                            {/*<ReadListBorder />*/}
-                            {/*<LatestPopularBorder />*/}
-                            {/*<LatestPopularBorder />*/}
+                            <div id="feed">
+                                <div class="feed-card highlight bb" style={{backGroundImage: "url(" + "https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ce1a28eea6506315f65db68/16036648996358347060.jpg" + ")"}}>
+                                    <div class="feed-highlight-badge-box"></div>
+                                    <div class="w-clearfix feed-card inheightlight animated fadeIn">
+                                        <img class="img-feed-highlight" src="https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ce1a28eea6506315f65db68/16036648996358347060.jpg" width="100%"></img>
+                                        <div class="feed-info nobg highlight">
+                                            <div class="w-clearfix feed-type">
+                                                <div class="feed-badge-small staffpick"></div>
+                                                <a class="" href="/category/shortstory">
+                                                    <img height="30px" src="/assets/1N83gyP.svg"></img></a>
+                                            </div>
+                                            <a href="/story/5ce1a28eea6506315f65db68">
+                                                <div class="feed-info-title">เธอ ร่างบิดเบี้ยว และแสงแดดจ้าตอน 2 ทุ่ม 15 นาที</div>
+                                                <div class="feed-info-detail">  ในที่สุดผมก็ร่วงหล่นไร้การหยิบจับ หลังจากพยายามเหนี่ยวรั้งให้ยึดโยงเลียนแบบสิ่งสวยงามที่ถูกสรรค์สร้างโดยธรรมชาติ หรือแม้กระทั่งโดยผู้คน ทันใดก็พังทลายลงมาเป็นเศษเสี้...</div>
+                                            </a>
+                                            <div class="w-clearfix feed-info-point">
+                                                <div class="text">
+                                                    <span>1 day ago</span><span>&nbsp;&nbsp;</span>
+                                                </div>
+                                                <span><span>|&nbsp;&nbsp;</span>
+                                                <div class="text orange">
+                                                    <span>2</span><span>&nbsp;</span>
+                                                </div>
+                                                <div class="text">recommended</div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* information about blog */}
+                            {/* information about blog */}
+                            <div id="feed">
+                                <div class="feed-card highlight bb" style={{backGroundImage: "url(" + "https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ce1a28eea6506315f65db68/16036648996358347060.jpg" + ")"}}>
+                                    <div class="feed-highlight-badge-box"></div>
+                                    <div class="w-clearfix feed-card inheightlight animated fadeIn">
+                                        <img class="img-feed-highlight" src="https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ce1a28eea6506315f65db68/16036648996358347060.jpg" width="100%"></img>
+                                        <div class="feed-info nobg highlight">
+                                            <div class="w-clearfix feed-type">
+                                                <div class="feed-badge-small staffpick"></div>
+                                                <a class="" href="/category/shortstory">
+                                                    <img height="30px" src="/assets/1N83gyP.svg"></img></a>
+                                            </div>
+                                            <a href="/story/5ce1a28eea6506315f65db68">
+                                                <div class="feed-info-title">เธอ ร่างบิดเบี้ยว และแสงแดดจ้าตอน 2 ทุ่ม 15 นาที</div>
+                                                <div class="feed-info-detail">  ในที่สุดผมก็ร่วงหล่นไร้การหยิบจับ หลังจากพยายามเหนี่ยวรั้งให้ยึดโยงเลียนแบบสิ่งสวยงามที่ถูกสรรค์สร้างโดยธรรมชาติ หรือแม้กระทั่งโดยผู้คน ทันใดก็พังทลายลงมาเป็นเศษเสี้...</div>
+                                            </a>
+                                            <div class="w-clearfix feed-info-point">
+                                                <div class="text">
+                                                    <span>1 day ago</span><span>&nbsp;&nbsp;</span>
+                                                </div>
+                                                <span><span>|&nbsp;&nbsp;</span>
+                                                <div class="text orange">
+                                                    <span>2</span><span>&nbsp;</span>
+                                                </div>
+                                                <div class="text">recommended</div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             {/* information about blog */}
                         </div>
                     </div>
