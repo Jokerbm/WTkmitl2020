044358ae9764a3a3fb3dfd15680eb89eff895f67
Lalitwadee <rari363970@gmail.com>
2020-11-10 20:02:24 +0700

    update footer click

--- storylog/src/components/ClickBorder.jsx
+++ storylog/src/components/ClickBorder.jsx
@@ -2,57 +2,138 @@ import React from "react";
 
 function ClickBorder() {
     return (
-        <div className="AfterClickReadlist">
-            {/* <!-- Button trigger modal --> */}
+        <div className="AfterClickReadlist th-font2">
             <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
                 AfterClickReadlist
             </button>
-
-            {/* <!-- Modal --> */}
             <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <span className="glyphicon"></span>
-                <div className="modal-dialog" role="document" id="content">
-                    <div className="modal-content">
-                        <div className="modal-header">
-
-                        </div>
-                        <div className="modal-body th-font2">
-                            ...ข้อความ...
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-                            เราทุกคน ย่อมมีเป้าหมายของตัวเองและเรารู้ว่าเราต้องทำอย่างไรต้องเดินอย่างไร แต่จะมีสักกี่เหตุผลที่ทำให้เราอยากเป็นไปตามเป้าหมายมันอาจจะหาไม่เจอวันนี้ แต่เราจะเจอเหตุผลในสักวัน ผมอยากให้ทุกคนที่ขึ้นมาบนเส้นทางแห่งฝันนี้ทำตามฝันของเราให้เสร็จ เพราะนี่คือชีวิตเรา ความฝันเรา เราไม่จำเป็นต้องโดนบังคับให้
-
-                        </div>
-                        <div className="modal-footer th-font2">
-                        </div>
-                        <div className="modal-footer th-font2">
-
-                        </div>
-                    </div>
-                </div>
-                <div className="modal-dialog" role="document" id="content">
-                    <div className="modal-content">
-                        <div className="modal-body">
-                            <div id="content">
-                                <p className=" register-form center th-font2">Comment</p>
-                                <input className="comment th-font2" type="text" placeholder="Say somethinh about this story..." size="80"></input>
+                <div id="actionBar">
+                    <div>
+                        <div className="w-clearfix content-footer animated fadeIn">
+                            <a className="" href="/OttoChain">
+                            <div className="content-share-profile_icon sizee"
+                                style={{ backgroundImage: "url(" + "https://s3-ap-southeast-1.amazonaws.com/media.storylog/usercontent/5e56a440a1d6b666713dd5c7/16022734106659680676.jpg"+")"}}></div>
+                            <div className="content-share-text text green">OttoChain
+                            </div>
+                            <div className="content-share-text"><span>&nbsp;&nbsp;</span><span>Tutor, Musician</span></div>
+                        </a>
+                            <div className="bb bb-edit-content"></div>
+                            <div className="bb content-share-share_bb">
+                                <div className="content-share-icon_share_bb recommend"></div>
+                                <div className="left content-share-bb_text recommend">Recommend
+                                </div>
+                            </div>
+                            <div className="bb content-share-share_bb bookmark">
+                                <div className="content-share-icon_share_bb bookmark"></div>
+                                <div className="left content-share-bb_text bookmark">Bookmark
+                                </div>
+                            </div>
+                            <div className="content-share-share_bb">
+                                <div className="content-share-icon_share_bb"></div>
+                                <div className="left content-share-bb_text">Share
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <button id="bb_close_modalStoryDetail" className="bb bb-close-popup animated fadeIn"></button>
             </div>
         </div>
+
     );
 }
 export default ClickBorder;
-
+{/* <span className="glyphicon"></span> */}
+// <div className="modal-dialog" role="document" id="content">
+//     <div className="modal-content">
+//         <div className="card-popup click">
+//             <div className="popup-border-title" id="exampleModalLong">
+//                 <img className="click-border" src={process.env.PUBLIC_URL + 'img/pogemon.jpg'} alt="test" />
+//             </div>
+//             <div className="register-form all">
+//                 <a class="" target="_self" href="#/category/short story"><img height="40px" alt="experience" src="img/shortstory.svg" /></a>
+//             </div>
+//             <div className="register-form center all">
+//                 <div className="tabs">
+//                     <div class="feed-badge-small staffpick border"></div>
+//                     <div className="tabs-text clickborder th-font2">Subject</div>
+//                     <div className="tabs-line login"></div>
+//                 </div>
+//                 <div className="text th-font2">เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                     เราใช้การล็อคอินผ่าน
+//                     Facebook เพราะเราให้ความสำคัญกับตัวตนของนักเขียน
+//                     อย่างไรก็ตามคุณสามารถแก้ไขเปลี่ยนชื่อและรูปภาพได้ภายหลังจากการสมัครทันที
+//                 </div>
+//             </div>
+//             <div className="popup-footer">
+//                 Published...
+//             </div>
+//             <div className="tabs">
+//                 <div className="tabs-line login"></div>
+//             </div>
+//             <div className="register-form center all">
+//                 <div className="row">
+//                     <div className="col profile">
+//                         <img className="profile-image_banner" src={process.env.PUBLIC_URL + 'img/pogemon.jpg'} alt="test" />
+//                     </div>
+//                     <div className="col">
+//                         <div className="row">Hello</div>
+//                         <div className="row green">Subject</div>
+//                         <div className="row">Tag</div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="tabs">
+//                 <div className="tabs-line login"></div>
+//             </div>
+//             <div className="register-form center all">
+//                 <div className="row">
+//                     <div className="col profile">
+//                         <img className="profile-user-image_user" src={process.env.PUBLIC_URL + 'img/pogemon.jpg'} alt="test" />
+//                     </div>
+//                     <div className="col">
+//                         <div className="row">Hello</div>
+//                         <div className="row">
+//                             <div className="col1 green"><b>My name</b></div>
+//                             <div className="col1">NickName</div>
+//                         </div>
+//                         <div className="row">Status</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+// <div className="modal-dialog" role="document" id="content">
+//     <div className="modal-content">
+//         <div className="modal-body">
+//             <div id="content">
+//                 <p className=" register-form center th-font2">Comment</p>
+//                 <input className="comment th-font2" type="text" placeholder="Say somethinh about this story..." size="80"></input>
+//             </div>
+//         </div>
+//     </div>
+// </div>
